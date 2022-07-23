597252f9cef44d048a21ef45988fd82085fd84f2
KrishivAnkitAgarwal29 <104824392+balakrishnan29@users.noreply.github.com>
2022-07-23 10:26:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+p {
+    color: red;
+    letter - spacing: 1 px;
+    font - size: 15 px;
+}
+span {
+    letter - spacing: 1 px;
+}#
+mobilenet {
+    letter - spacing: 2 px;
+}
+.bottom {
+    color: red;
+    position: fixed;
+    bottom: 0;
+    text - align: center;
+    width: 100 % ;
+    font - family: 'Roboto Slab',
+    serif;
+}
+.img {
+    margin - top: 10 px;
+    margin - bottom: 10 px;
+    height: 300 px;
+    border - radius: 10 px;
+}
+b {
+    color: purple;
+}
+.first, .second {
+    background - color: white;
+    border - radius: 10 px;
+    line - height: 2;
+}
+.test_image_and_result1, .test_image_and_result2, .test_image_and_result3, .test_image_and_result4, .test_image_and_result5, .test_image_and_result6, .test_image_and_result7, .test_image_and_result8, .test_image_and_result9 {
+    margin - top: 15 px;
+    background - color: rgb(155, 153, 153);
+    border - radius: 10 px;
+    box - shadow: 1 px 1 px 25 px grey;
+    width: 99 % ;
+    margin - bottom: 15 px;
+}
+.white1, .white2, .white3, .white4, .white5, .white6, .white7, .white8, .white9 {
+    color: white;
+}
+span {
+    color: red;
+}
+.google_lens {
+    color: black;
+}
+h4 {
+    color: white;
+    font - weight: bold;
+}
